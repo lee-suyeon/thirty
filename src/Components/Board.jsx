d0653fe0bc02a02f862e4174dafe8fbc3ada3189
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-02 12:02:47 +0900

    Create Board Component
    1. 보드 위에 add 버튼과 메세지

---
+++ src/Components/Board.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../Style/Theme';
+import { AddCircleOutline } from '@styled-icons/material';
+
+const BackBoard = styled.div`
+  width: 100%;
+  display: flex;
+  height: 68vh;
+  position: relative;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px 5px rgba(0, 0, 0, 0.02);
+`
+const AddMessage = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60%;
+  line-height: 1.4;
+  font-weight: 500;
+  text-align: center;
+`
+const AddButton = styled(AddCircleOutline)`
+  display: block;
+  fill: ${({ theme }) => theme.colors.subColor};
+  width: 70px;
+  margin: 0 auto 0.5rem;
+  cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    fill:  ${({ theme }) => theme.colors.mainColor};
+  }
+`
+
+const Board = ({ children }) => {
+   return (
+      <>
+         <BackBoard>
+            <AddMessage>                  
+               <AddButton/>
+               There are no challenge yet.<br /> Click + button to add your first challenge.
+            </AddMessage>
+         </BackBoard>
+      </>
+   )
+}
+
+export default Board;
