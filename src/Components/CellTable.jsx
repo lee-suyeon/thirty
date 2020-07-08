ceda0048de2cfb3c0362353d4ada63ddef420994
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-08 22:05:52 +0900

    Fix days
    1. 원래는 App 컴포넌트에서 props를 받아왔는데 그럴 필요가 없을거 같아서..

--- src/Components/CellTable.jsx
+++ src/Components/CellTable.jsx
@@ -5,12 +5,12 @@ import Cell from './Cell';
 const RightBox = styled.div`
     width: 65%;
 `
-//const dayNumbers = Array(30).fill().map((v, i) => i + 1);
+const dayNumbers = Array(30).fill().map((v, i) => i + 1);
 
-const CellTable = ({ dispatch, onCellCount, days }) => {
+const CellTable = ({ dispatch, onCellCount, dday, checkState }) => {
     return (
         <RightBox>
-            {days.map((v, i) => <Cell key={`${i}days`} days={v} dispatch={dispatch} onCellCount={onCellCount} />)}
+            {dayNumbers.map((v, i) => <Cell key={`${i}days`} days={v} dispatch={dispatch} dday={dday} checkState={checkState} onCellCount={onCellCount} />)}
         </RightBox>
     )
 }
