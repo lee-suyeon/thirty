32a941e5f9a881144482d68937add313d50f0a02
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-11 21:55:15 +0900

    Updates

---
+++ precache-manifest.2a2145961126bbda1601353f12f580e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3388fdb55093865c2d8f382c8968646",
+    "url": "/30days/index.html"
+  },
+  {
+    "revision": "c33f0c2b0d9b4faafcba",
+    "url": "/30days/static/js/2.4fb69480.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/30days/static/js/2.4fb69480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d303067873cbbbebaab7",
+    "url": "/30days/static/js/main.0de85400.chunk.js"
+  },
+  {
+    "revision": "58dbd2d1ffad0753f81d",
+    "url": "/30days/static/js/runtime-main.0825b22b.js"
+  }
+]);
