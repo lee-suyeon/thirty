004c2f374e990f25351bdaf21b8c04e683e82fb0
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-11 21:29:16 +0900

    Add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,6 @@ const App = () => {
             cellState={cellState}
             />
           <CheckMessage/>
-
         </Template>
         <StartChallenge 
           visible={modal.form}/>
